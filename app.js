b92a626754abf11f41b5fba6f3b99cf7474c8c49
kazimtekin <78320314+kazimtekin@users.noreply.github.com>
2021-01-31 22:47:01 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('ODA0MzcwOTgxOTA4MzgxNzE2.YBLW2g.StW5FEAQDsxxYY0fYlN02o9RA2M');
